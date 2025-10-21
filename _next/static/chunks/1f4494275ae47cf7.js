93a278bc221710a8fd2cc8635a6d5698e51a0768
KALEMA ABUBAKAR <kibogahillfarmuganda@gmail.com>
2025-10-22 00:04:59 +0300

    Add files via upload

---
+++ _next/static/chunks/1f4494275ae47cf7.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/c839ea512d21cec9.js",
+  "static/chunks/6a7c1b3ba8604ff5.js",
+  "static/chunks/turbopack-50ca5b843e3e0c42.js"
+])
---
+++ _next/static/chunks/9dd11df5b1560c47.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/0524dd42d3e6e5b2.js",
+  "static/chunks/6a7c1b3ba8604ff5.js",
+  "static/chunks/turbopack-b4c8642cdcc19db2.js"
+])
